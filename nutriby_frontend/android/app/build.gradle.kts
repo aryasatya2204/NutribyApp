2d1da8ec2d13863c36bdae730db0c8de3abdb827
yazeedilmi <yazidilmi233@gmail.com>
2025-12-17 00:40:13 +0700

    add keystore

--- nutriby_frontend/android/app/build.gradle.kts
+++ nutriby_frontend/android/app/build.gradle.kts
@@ -30,6 +30,22 @@ android {
         versionName = flutter.versionName
     }
 
+    val keystorePropertiesFile = rootProject.file("key.properties")
+    val keystoreProperties = java.util.Properties()
+
+    if (keystorePropertiesFile.exists()) {
+        keystoreProperties.load(java.io.FileInputStream(keystorePropertiesFile))
+    }
+
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
+
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
