8ac7af2f08dbf66ea9e71a2047db226941038f8f
arya_ganteng <sitialysa405@example.com>
2025-12-17 08:55:43 +0700

    atur path

--- nutriby_frontend/lib/models/recipe.dart
+++ nutriby_frontend/lib/models/recipe.dart
@@ -22,7 +22,7 @@ class Recipe {
     if (imageUrl == null || imageUrl!.isEmpty) return null;
     // Sesuaikan base URL
     // return 'http://10.0.2.2:8000/storage/$imageUrl';
-    return 'https://nutribyapp.user.cloudjkt02.com/public/recipes/$imageUrl';
+    return 'https://nutribyapp.user.cloudjkt02.com/recipes/$imageUrl';
   }
 
   Recipe({
